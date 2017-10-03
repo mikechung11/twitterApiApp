61be4db27bfb9ea5af2be82db7e44551aa71cc06
Jason Otter <jasonotter17@gmail.com>
2017-10-02 18:20:23 -0700

    working on service and api controller, just checking in

---
+++ Services/TwitterService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using twitterApp.Models.Domain;
+
+namespace twitterApp.Services
+{
+    public class TwitterService
+    {
+        private string pbToken;
+
+        public string GetPostBearerToken(string encodedKey)
+        {
+            WebClient client = new WebClient();
+            client.Headers[HttpRequestHeader.Authorization] = "Basic " + encodedKey;
+            client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded;charset=UTF-8";
+            string data = "grant_type=client_credentials";
+            string response = client.UploadString("https://api.twitter.com/oauth2/token", data);
+            pbToken = response;
+            return response;
+        }
+
+        public string GetUserTimeline(TwitterUser user)
+        {
+            WebClient client = new WebClient();
+            client.Headers[HttpRequestHeader.Authorization] = "Bearer " + pbToken;
+            String link = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=10&screen_name=" + user.screenName;
+            string response = client.DownloadString(link);
+
+            return response;
+        }
+    }
+}
---
+++ Web/Controllers/ApiControllers/TwitterApiController.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using twitterApp.Models.Domain;
+using twitterApp.Services;
+
+namespace twitterApp.Web.Controllers.ApiControllers
+{
+    [RoutePrefix("api/twitter")]
+    public class TwitterApiController : ApiController
+    {
+        private TwitterService _twitterService = new TwitterService();
+
+        [Route(), HttpGet]
+        public HttpResponseMessage GetPostBearerToken()
+        {
+            string apiKey = "sfUO4NiHYY0VljcV2Hh3sh5sa";
+            string apiSecret = "b357KciJinhRTPKYr2m3FxTAFqCZdVP5r1cCm7Dffl78pOTpFh";
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(apiKey + ":" + apiSecret);
+            var encodedKey = System.Convert.ToBase64String(plainTextBytes);
+
+            try
+            {
+                string response = _twitterService.GetPostBearerToken(encodedKey);
+                JObject jResponse = JObject.Parse(response);
+                string token = jResponse["access_token"].ToString();
+                return Request.CreateResponse(HttpStatusCode.OK, token);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [Route("{screenName}"), HttpGet]
+        public HttpResponseMessage GetUserTimeline(string screenName)
+        {
+            TwitterUser user = new TwitterUser();
+            user.screenName = screenName;
+
+            try
+            {
+                string response = _twitterService.GetUserTimeline(user);
+                JArray jResponse = JArray.Parse(response);
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
